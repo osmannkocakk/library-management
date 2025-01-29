64bf120cdb232cc561ffdbe33c1f371262ea7785
Osman Koçak <osman.kocak@gmail.com>
2025-01-29 10:05:55 +0300

    Initializing project with NodeJs, TypeScript, Express dependencies

---
+++ scripts/db_scripts.sql
@@ -0,0 +1,21 @@
+CREATE TABLE IF NOT EXISTS users (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(255) NOT NULL,
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE IF NOT EXISTS books (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(255) NOT NULL,
+  average_rating FLOAT DEFAULT 0,
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE IF NOT EXISTS borrow_records (
+  id SERIAL PRIMARY KEY,
+  user_id INT REFERENCES users(id),
+  book_id INT REFERENCES books(id),
+  borrowed_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  returned_at TIMESTAMP,
+  rating INT CHECK (rating >= 0 AND rating <= 10)
+);
